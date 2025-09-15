4bf6e37c3fb03f2597fdcc2112be2c55fcbd477a
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-09-15 17:43:28 +0530

    socket.io setup in server

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 
@@ -47,6 +48,30 @@ export const markMessageAsSeen =async(req,res)=>{
         const {id}= req.params;
         await Message.findByIdAndUpdate(id,{seen:true});
         res.json({success:true})
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success:false,message:error.message});
+    }
+}
+
+//send message
+export const sendMessage = async(req,res)=>{
+    try {
+        const {text,image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl,
+        })
+        res.json({success:true,newMessage});
     } catch (error) {
         console.log(error.messages);
         res.json({success:false,message:error.message});
